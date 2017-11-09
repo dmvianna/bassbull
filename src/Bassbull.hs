68a4a706a846f0265b1afaafb2c98d8f70609446
Daniel Vianna <daniel.vianna@dpc.vic.gov.au>
2017-11-09 11:26:04 +1100

    fix tabs

--- src/Bassbull.hs
+++ src/Bassbull.hs
@@ -2,23 +2,23 @@
 
 module Bassbull where
   
-  import qualified Data.ByteString.Lazy as BL
-  import qualified Data.Foldable as F
-  import Data.Csv.Streaming
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Foldable as F
+import Data.Csv.Streaming
   
-  type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+baseballStats :: BL.ByteString -> Records BaseballStats
+baseballStats = decode NoHeader
   
-  baseballStats :: BL.ByteString -> Records BaseballStats
-  baseballStats = decode NoHeader
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, d) = d
   
-  fourth :: (a, b, c, d) -> d
-  fourth (_, _, _, d) = d
+summer :: (a, b, c, Int) -> Int -> Int
+summer = (+) . fourth
   
-  summer :: (a, b, c, Int) -> Int -> Int
-  summer = (+) . fourth
-  
-  -- FilePath is just an alias for String
-  getAtBatsSum :: FilePath -> IO Int
-  getAtBatsSum battingCsv = do
-    csvData <- BL.readFile battingCsv
-    return $ F.foldr summer 0 (baseballStats csvData)
+-- FilePath is just an alias for String
+getAtBatsSum :: FilePath -> IO Int
+getAtBatsSum battingCsv = do
+  csvData <- BL.readFile battingCsv
+  return $ F.foldr summer 0 (baseballStats csvData)
